71a640b798dc5e64116d4422a3e6ec6f73da9453
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-09-29 21:38:25 +0300

    new Wireframe

--- software-eng/se.js
+++ software-eng/se.js
@@ -26,3 +26,9 @@ var chart = new Chart(ctx, {
     }
 
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
